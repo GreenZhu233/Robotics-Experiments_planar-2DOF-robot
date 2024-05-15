11d6700e6a99889b3579e4012273655e7bcdd24b
GreenZhu233 <2546594311@qq.com>
2024-05-15 21:45:19 +0800

    添加了主动杆角度导出数据功能，供建模软件使用

--- main.m
+++ main.m
@@ -1,5 +1,11 @@
 %% 生成矩形
-trace = createRectangle(79, 104, 10, 125, 200, 1);
+lx = 79;    % 矩形的宽度
+ly = 104;   % 矩形的高度
+R = 10;     % 圆角半径
+Mx = 125;   % 矩形中心横坐标
+My = 200;   % 矩形中心纵坐标
+d = 1;      % 相邻两C点的距离
+trace = createRectangle(lx, ly, R, Mx, My, d);
 
 %% 杆长赋值
 r1 = 236;
@@ -32,46 +38,61 @@ D = E - [r4*cos(theta_4), r4*sin(theta_4)];
 C1 = B + [r2*cos(theta_2), r2*sin(theta_2)];
 C2 = D - [r3*cos(theta_3), r3*sin(theta_3)];
 
+%% 导出杆1和杆4角度数据
+v = 10;     % C点运动速度
+delta_t = d / v;
+timeSeries = (0 : delta_t : (size-1)*delta_t)';     % 生成时间序列
+link1 = [timeSeries, theta_1];          % 杆1角度
+link4 = [timeSeries, theta_4 + pi];     % 杆4角度(将相对D点的角度转化为相对E点的角度)
+writematrix(link1, "link1.csv");
+writematrix(link4, "link4.csv");
+
 %% 模拟动画
-filename = 'MatlabAnimation1.gif';
-figure('Position', [100, 100, 800, 800]);
-for i = 1:size
-    % 清除上一帧
-    clf;
+createGif(trace, A, B, C1, C2, D, E);
 
-    % 绘制点轨迹
-    scatter(trace(:,1), trace(:,2), 5, "g", "LineWidth", 1);
-    hold on;
-
-    % 绘制四根杆
-    plot([A(i,1) B(i,1)], [A(i,2) B(i,2)], 'r', 'LineWidth', 4); % 杆1
-    hold on;
-    plot([B(i,1) C1(i,1)], [B(i,2) C1(i,2)], 'b', 'LineWidth', 4); % 杆2
-    hold on;
-    plot([C2(i,1) D(i,1)], [C2(i,2) D(i,2)], 'b', 'LineWidth', 4); % 杆3
-    hold on;
-    plot([D(i,1) E(i,1)], [D(i,2) E(i,2)], 'r', 'LineWidth', 4); % 杆4
-    hold on;
-    scatter(C1(i,1), C1(i,2), 10, "black", "LineWidth", 2);
-    hold on;
-
-    % 设置坐标轴
-    xlim([-40,290]);
-    ylim([-40,290]);
-    daspect([1 1 1]);
-
-    % 保存动画
-    frame = getframe(gcf);
-    im = frame2im(frame);
-    [imind, cm] = rgb2ind(im, 256);
-
-    % 写入GIF文件
-    if i == 1
-        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', 0.1);
-    else
-        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
+%% 生成动画Gif
+function createGif(trace, A, B, C1, C2, D, E)
+    filename = 'MatlabAnimation.gif';
+    figure('Position', [100, 100, 800, 800]);
+    size = length(trace);
+    for i = 1:size
+        % 清除上一帧
+        clf;
+    
+        % 绘制点轨迹
+        scatter(trace(:,1), trace(:,2), 5, "g", "LineWidth", 1);
+        hold on;
+    
+        % 绘制四根杆
+        plot([A(i,1) B(i,1)], [A(i,2) B(i,2)], 'r', 'LineWidth', 4); % 杆1
+        hold on;
+        plot([B(i,1) C1(i,1)], [B(i,2) C1(i,2)], 'b', 'LineWidth', 4); % 杆2
+        hold on;
+        plot([C2(i,1) D(i,1)], [C2(i,2) D(i,2)], 'b', 'LineWidth', 4); % 杆3
+        hold on;
+        plot([D(i,1) E(i,1)], [D(i,2) E(i,2)], 'r', 'LineWidth', 4); % 杆4
+        hold on;
+        scatter(C1(i,1), C1(i,2), 10, "black", "LineWidth", 2);
+        hold on;
+    
+        % 设置坐标轴
+        xlim([-40,290]);
+        ylim([-40,290]);
+        daspect([1 1 1]);
+    
+        % 保存动画
+        frame = getframe(gcf);
+        im = frame2im(frame);
+        [imind, cm] = rgb2ind(im, 256);
+    
+        % 写入GIF文件
+        if i == 1
+            imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', 0.1);
+        else
+            imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
+        end
+    
+        % 暂停以控制动画速度
+        pause(0.1);
     end
-
-    % 暂停以控制动画速度
-    pause(0.1);
 end
